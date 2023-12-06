000304764f9182f633d8ccbd6829759065d892a3
Cecilialzq <115562180+Cecilialzq@users.noreply.github.com>
2023-12-06 10:16:26 +0000

    Add files via upload

---
+++ rtl/pc/pc.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# cleanup
+
+rm -rf obj_dir
+em -f pc.vcd
+
+#run Verilator to translate Verilog into C++
+verilator -Wall --cc --trace pc.sv --exe pc_tb.cpp
+ 
+make -j -C obj_dir/ -f Vpc.mk Vpc
+# run executable simulation file
+obj_dir/Vpc
