452bc94a462b030d7d3a3bb69ea81ba6e1856a59
Cecilialzq <Cecilialzq@github.com>
2023-12-07 10:48:52 +0000

    Fixed bugs and get simulation results

--- rtl/pc/pc_tb.cpp
+++ rtl/pc/pc_tb.cpp
@@ -1,6 +1,6 @@
-#include "Vpc.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
+#include"Vpc.h"
 
 
 int main(int argc, char **argv, char** env){
@@ -9,59 +9,59 @@ int main(int argc, char **argv, char** env){
     Verilated::commandArgs(argc, argv);
 
     // init top instance
-    Vpc *top = new Vpc();
+    Vpc *top = new Vpc;
 
     // init trace dump of signals
     Verilated::traceEverOn(true);
     VerilatedVcdC *tfp = new VerilatedVcdC;
     top->trace(tfp, 99);
-    tfp->open("pc/pcwaveform.vcd");
+    tfp->open("pc.vcd");
 
     // init inputs and outputs
-    top->clk = 1;
-    top->rst = 1;
+    //top->clk = 1;
+   // top->rst = 1;
     top->pc_4 =0;
     top->pc_b =0;
     top->pc_r =0;
-    top->pc_next =0;
+    top->PC =0;
    for(i=0;i<300;i++){
        for(clk = 0; clk < 2; clk++){
             tfp->dump(2*i + clk);
             top->clk = !top->clk;
             top->eval();
-        }
- }
-
-     if(i==2){
+        } 
+        
+        if(i==2){
         //for branch instruction
-        top->pc_src=2'b1;
-        top->pc_4=0x00000004;
-        top->pc_b=0x00000018;
-        top->pc_r=0x00000024;
-        //pc_next should be 0x00000018
+        top->pc_src = 1;
+        top->pc_4 =0x014;
+        top->pc_b =0x00C;
+        top->pc_r =0x001;
+        //pc_next should be 0x00C
      }
      else if(i==3){
      //for register instruction
-        top->pc_src=2'b10;
-        top->pc_4=0x00000010;
-        top->pc_b=0x0000000C;
-        top->pc_r=0x00000020;
-        //pc_next should be 0x00000020
+        top->pc_src = 2;
+        top->pc_4 =0x010;
+        top->pc_b =0x00C;
+        top->pc_r =0x020;
+        //pc_next should be 0x020
 
      }
      else if(i==4){
        //for adding 4 instruction
-        top->pc_src=2'b??;
-        top->pc_4=0xFFFFFFF4;
-        top->pc_b=0xFFFFFFEC;
-        top->pc_r=0x0000001C;
-        //pc_next should be 0xFFFFFFF4
+        top->pc_4 =0xFF4;
+        top->pc_b =0xFEC;
+        top->pc_r =0x1C;
+        //pc_next should be 0xFF4
      }
+ }
+
+
 
     if(Verilated::gotFinish()) exit(0);
 
 
     tfp->close();
     exit(0);
-    std::cout<<pc_next<<std::endl;
 }
